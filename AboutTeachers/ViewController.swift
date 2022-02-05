21ff7b42a85923771f2b631141ed9ce37c48785c
Denis <varenikovda@gmail.com>
2022-02-05 13:42:49 +0300

    Initial Commit

---
+++ AboutTeachers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AboutTeachers
+//
+//  Created by Денис on 05.02.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
